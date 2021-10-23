1a03e41ceab49307c84a8507856adb86d3223082
Carlos Tacuri <carlosxtacuri@gmail.com>
2021-10-23 17:42:48 -0500

    cambio

--- azure-terraform/kubernetes/variables.tf
+++ azure-terraform/kubernetes/variables.tf
@@ -17,13 +17,13 @@ variable node_count {
 
 
 variable dns_prefix {
-  default = "k8sAforo255sv"
+  default = "k8sAforo255cx"
 }
 
 variable cluster_name {
-  default = "k8sAforo255sv"
+  default = "k8sAforo255cx"
 }
 
 variable resource_group {
-  default = "kubernetesAforo255"
+  default = "kubernetesAforo255cx"
 }
